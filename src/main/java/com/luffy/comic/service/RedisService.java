397ac3aa7a709bd247bf7f0661a0709d40cdf39a
luffy <2523131653@qq.com>
2019-08-08 22:52:42 +0800

    添加redis服务和测试。

---
+++ src/main/java/com/luffy/comic/service/RedisService.java
@@ -0,0 +1,32 @@
+package com.luffy.comic.service;
+
+/**
+ * redis操作Service
+ */
+public interface RedisService {
+    /**
+     * 存储数据
+     */
+    void set(String key, String value);
+
+    /**
+     * 获取数据
+     */
+    String get(String key);
+
+    /**
+     * 设置过期时间
+     */
+    boolean expire(String key, long expire);
+
+    /**
+     * 删除数据
+     */
+    void remove(String key);
+
+    /**
+     * 自增操作
+     * @param delta 步长
+     */
+    Long increment(String key, long delta);
+}
