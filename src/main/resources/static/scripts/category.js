a094892e2c94a10e85c79212d9b75a4327775e37
luffy <2523131653@qq.com>
2020-08-07 10:23:13 +0800

    删掉多余的static文件加小修补

--- src/main/resources/static/scripts/category.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-    const requests = getRequest(null);
-    let pos = requests['categoryId'] === undefined ? 1 : parseInt(requests['categoryId']);
-    $('#nav-category a').eq(pos - 1).css('color', 'blue')
-});
